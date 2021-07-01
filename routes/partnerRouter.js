b8af976c9622cec772b531c71776256f200bdb59
Jarred Oceguera <jarred_ro@hotmail.com>
2021-07-01 13:02:51 -0700

    Basic Authentication

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -3,6 +3,74 @@ const Partner = require('../models/partner');
 
 const partnerRouter = express.Router();
 
+partnerRouter.route('/')
+    .get((req, res, next) => {
+        Partner.find()
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner.comments);
+            })
+            .catch(err => next(err));
+    })
+    .post((req, res, next) => {
+        Partner.create(req, body)
+            .then(partner => {
+                console.log('Partner Created', partner);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end(`PUT operation not supported on /partners/${req.params.partnerId}/comments`);
+    })
+    .delete((req, res, next) => {
+        Partner.deleteMany()
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    });
+
+partnerRouter.route('/:partnerId')
+    .get((req, res, next) => {
+        Partner.findById(req.params.partnerId)
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /partners/${req.params.partnerId}');
+    })
+    .put((req, res, next) => {
+        partner.findByIdAndUpdate(req.params.partnerId, {
+            $set: req.body
+        }, { new: true })
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .delete((req, res, next) => {
+        Partner.findById(req.params.partnerId)
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    });
 partnerRouter.route('/:partnerId/comments')
     .get((req, res, next) => {
         Partner.findById(req.params.partnerId)
@@ -65,5 +133,4 @@ partnerRouter.route('/:partnerId/comments')
             })
             .catch(err => next(err));
     });
-
 module.exports = partnerRouter;
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -3,6 +3,74 @@ const Promotion = require('../models/promotion');
 
 const promotionRouter = express.Router();
 
+promotionRouter.route('/')
+    .get((req, res, next) => {
+        Promotion.find()
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion.comments);
+            })
+            .catch(err => next(err));
+    })
+    .post((req, res, next) => {
+        Promotion.create(req, body)
+            .then(promotion => {
+                console.log('Promotion Created', promotion);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end(`PUT operation not supported on /promotions/${req.params.promotionId}/comments`);
+    })
+    .delete((req, res, next) => {
+        Promotion.deleteMany()
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+    });
+
+promotionRouter.route('/:promotionId')
+    .get((req, res, next) => {
+        Promotion.findById(req.params.promotionId)
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /promotions/${req.params.promotionId}');
+    })
+    .put((req, res, next) => {
+        promotion.findByIdAndUpdate(req.params.promotionId, {
+            $set: req.body
+        }, { new: true })
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+    })
+    .delete((req, res, next) => {
+        Promotion.findById(req.params.promotionId)
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+    });
 promotionRouter.route('/:promotionId/comments')
     .get((req, res, next) => {
         Promotion.findById(req.params.promotionId)
@@ -65,5 +133,4 @@ promotionRouter.route('/:promotionId/comments')
             })
             .catch(err => next(err));
     });
-
 module.exports = promotionRouter;
