4a71f658db06b12b758a1f97a90f6445a2c6a7d2
Jarred Oceguera <jarred_ro@hotmail.com>
2021-07-02 17:19:04 -0700

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -7,17 +7,12 @@ const campsiteRouter = express.Router();
 
 campsiteRouter.route('/')
     .get((req, res, next) => {
-        Campsite.findById(req.params.campsiteId)
-            .then(campsite => {
-                if (campsite) {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(campsite.comments);
-                } else {
-                    err = new Error(`Campsite ${req.params.campsiteId} not found`);
-                    err.status = 404;
-                    return next(err);
-                }
+        Campsite.find()
+            .populate('comments.author')
+            .then(campsites => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(campsites);
             })
             .catch(err => next(err));
     })
@@ -48,6 +43,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
+            .populate('comments.author')
             .then(campsite => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -82,6 +78,7 @@ campsiteRouter.route('/:campsiteId')
 campsiteRouter.route('/:campsiteId/comments')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
+            .populate('comments.author')
             .then(campsite => {
                 if (campsite) {
                     res.statusCode = 200;
@@ -99,6 +96,7 @@ campsiteRouter.route('/:campsiteId/comments')
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
                 if (campsite) {
+                    req.body.author = req.user._id;
                     campsite.comments.push(req.body);
                     campsite.save()
                         .then(campsite => {
@@ -143,6 +141,7 @@ campsiteRouter.route('/:campsiteId/comments')
         campsiteRouter.route('/:campsiteId/comments/:commentId')
             .get((req, res, next) => {
                 Campsite.findById(req.params.campsiteId)
+                    .populate('comments.author')
                     .then(campsite => {
                         if (campsite) {
                             res.statusCode = 200;
