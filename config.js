b3f3b5bdd6135b74afc7d00a7c21cec4c9e66aaa
Jarred Oceguera <jarred_ro@hotmail.com>
2021-07-10 08:50:06 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': 1202609163485581, 0348c3bc8ded3b4a20882cfccd03d8c7{
+    clientId: 'YOUR FACEBOOK APP ID HERE',
+        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
+}
 }
